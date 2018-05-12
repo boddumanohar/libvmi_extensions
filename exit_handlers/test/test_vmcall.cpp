34b2faf979774f5e6c3294b69c37cfb67149a5b4
Manohar Reddy <201451024@iiitvadodara.ac.in>
2018-05-12 22:59:42 +0530

    init get_register_data

--- exit_handlers/test/test_vmcall.cpp
+++ exit_handlers/test/test_vmcall.cpp
@@ -20,6 +20,8 @@
 #include <bfvmm/hve/arch/intel_x64/vcpu/vcpu.h>
 #include <bfvmm/hve/arch/intel_x64/exit_handler/exit_handler.h>
 #include <bfdebug.h>
+#include <bfvmm/memory_manager/buddy_allocator.h>
+#include "vm_event.h"
 
 namespace libvmi
 {
@@ -46,11 +48,61 @@ public:
         );
     }
 
-    bool vmcall_handler(gsl::not_null<bfvmm::intel_x64::vmcs *> vmcs)
-    {
-        bfdebug_info(0, "vmcall handled");
-        return advance(vmcs);
-    }
+			bool vmcall_handler(gsl::not_null<bfvmm::intel_x64::vmcs *> vmcs) {
+				//hypercall 1 = test bareflank status
+				uint64_t id = vmcs->save_state()->rax; 
+			  if (id == 1) {
+					bfdebug_info(0, "vmcall handled");
+				}
+
+				if (id == 2) {
+					//get_register_data(vmcs);
+					bfdebug_info(0, "gettting register data");
+					uint64_t ptr1 = vmcs->save_state()->rdx;
+					vm_event_response_t *ptr = (vm_event_response_t *)ptr1;
+
+					if(ptr == NULL) {
+						bfdebug_info(0, "ptr is null");
+					}
+
+					ptr->version = 0;
+					ptr->reason = VM_EVENT_REGISTER; 
+					ptr->vcpuid = 1;
+					ptr->u.x86.rcx =  vmcs->save_state()->rcx;
+					ptr->u.x86.rdx =  vmcs->save_state()->rdx;
+					ptr->u.x86.rbx =  vmcs->save_state()->rbx;
+					ptr->u.x86.rsp =  vmcs->save_state()->rsp;
+					ptr->u.x86.rbp =  vmcs->save_state()->rbp;
+					ptr->u.x86.rsi =  vmcs->save_state()->rsi;
+					ptr->u.x86.rdi =  vmcs->save_state()->rdi;
+
+				}
+
+				return advance(vmcs);
+			}
+
+			void get_register_data(gsl::not_null<bfvmm::intel_x64::vmcs *> vmcs) {
+
+        bfdebug_info(0, "gettting register data");
+				uint64_t ptr1 = vmcs->save_state()->rdx;
+				vm_event_response_t *ptr = (vm_event_response_t *)ptr1;
+
+				if(ptr == NULL) {
+					bfdebug_info(0, "ptr is null");
+				}
+
+				ptr->version = 0;
+				ptr->reason = VM_EVENT_REGISTER; 
+				ptr->vcpuid = 1;
+				ptr->u.x86.rcx =  vmcs->save_state()->rcx;
+				ptr->u.x86.rdx =  vmcs->save_state()->rdx;
+				ptr->u.x86.rbx =  vmcs->save_state()->rbx;
+				ptr->u.x86.rsp =  vmcs->save_state()->rsp;
+				ptr->u.x86.rbp =  vmcs->save_state()->rbp;
+				ptr->u.x86.rsi =  vmcs->save_state()->rsi;
+				ptr->u.x86.rdi =  vmcs->save_state()->rdi;
+
+		}
 
     /// Destructor
     ///
