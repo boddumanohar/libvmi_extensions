a8e4f091d53ccfe44faaeb86dd97a97df5f905ee
Manohar Reddy <201451024@iiitvadodara.ac.in>
2018-05-21 10:55:16 +0530

    make dump does not work

--- exit_handler/main/vmcall_handler.cpp
+++ exit_handler/main/vmcall_handler.cpp
@@ -1,5 +1,4 @@
-//
-// Bareflank Extended APIs
+// // Bareflank Extended APIs
 // Copyright (C) 2018 Assured Information Security, Inc.
 //
 // This library is free software; you can redistribute it and/or
@@ -21,7 +20,6 @@
 #include <bfvmm/hve/arch/intel_x64/exit_handler/exit_handler.h>
 #include <bfdebug.h>
 #include <bfvmm/memory_manager/buddy_allocator.h>
-#include <bfjson.h>
 
 namespace libvmi
 {
@@ -44,53 +42,18 @@ public:
     {
         exit_handler()->add_handler(
 						intel_x64::vmcs::exit_reason::basic_exit_reason::vmcall,
-            handler_delegate_t::create<vcpu, &vcpu::vmcall_handler>(this)
+            handler_delegate_t::create<vcpu, &vcpu::_vmcall_handler>(this)
         );
     }
 
-			bool vmcall_handler(gsl::not_null<bfvmm::intel_x64::vmcs *> vmcs) {
-				//hypercall 1 = test bareflank status
-				/*uint64_t id = vmcs->save_state()->rax; 
-			  if (id == 1) {
+			bool _vmcall_handler(gsl::not_null<bfvmm::intel_x64::vmcs *> vmcs) {
+				
 					bfdebug_info(0, "vmcall handled");
-				}
-
-				if (id == 2) { */
-					//get_register_data(vmcs);
-
-					bfdebug_info(0, "gettting register data");
-					json j;
-					j["rax"] =  vmcs->save_state()->rax;
-					j["rbx"] =  vmcs->save_state()->rbx;
-					j["rcx"] =  vmcs->save_state()->rcx;
-					j["rdx"] =  vmcs->save_state()->rdx;
-					j["rsi"] =  vmcs->save_state()->rsi;
-					j["rdi"] =  vmcs->save_state()->rdi; 
-					j["rsp"] =  vmcs->save_state()->rsp;
-					j["rbp"] =  vmcs->save_state()->rbp;
-					j["rip"] =  vmcs->save_state()->rip;
-					j["r08"] =  vmcs->save_state()->r08;
-					j["r09"] =  vmcs->save_state()->r09;
-					j["r10"] =  vmcs->save_state()->r10;
-					j["r11"] =  vmcs->save_state()->r11;
-					j["r12"] =  vmcs->save_state()->r12;
-					j["r13"] =  vmcs->save_state()->r13;
-					j["r14"] =  vmcs->save_state()->r14;
-					j["r15"] =  vmcs->save_state()->r15;
-
-					std::string str = j.dump();
-					const char *cstr = str.c_str();
-					_putin_eax(cstr);
-
-			//}
-
-				return advance(vmcs);
+					BFDEBUG("%s\n", "HELLO WORLD");
+					return advance(vmcs);
 			}
 
-			void get_register_data(gsl::not_null<bfvmm::intel_x64::vmcs *> vmcs) {
-
-			}
-
+		
     /// Destructor
     ///
     /// @expects
