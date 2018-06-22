f0d01374a465184c1f32b8a79b98fc4ac92b5986
Manohar <201451024@iiitvadodara.ac.in>
2018-06-22 14:58:09 +0530

    enable apt when libvmi_extensions are initlized

--- exit_handler/vmcall_handler.cpp
+++ exit_handler/vmcall_handler.cpp
@@ -27,24 +27,175 @@
 #include <eapis/hve/arch/intel_x64/hve.h>
 #include <eapis/hve/arch/intel_x64/vic.h>
 #include <eapis/hve/arch/intel_x64/ept.h>
+#include <eapis/vcpu/arch/intel_x64/vcpu.h>
 
 using nlohmann::json;
 using namespace eapis::intel_x64;
 using namespace eapis::intel_x64::ept;
+
+namespace ept = eapis::intel_x64::ept;
+namespace vmcs = ::intel_x64::vmcs;
+
+const uint64_t page_size_bytes = 0x40000000ULL;
+const uint64_t page_count = 0x40ULL;
+
 namespace libvmi
 {
 
-auto mem_map = std::make_unique<eapis::intel_x64::ept::memory_map>();
 
-class vcpu : public bfvmm::intel_x64::vcpu
+class vcpu : public eapis::intel_x64::vcpu
+//class vcpu : public bfvmm::intel_x64::vcpu, public eapis::intel_x64::vcpu
 {
+private:
+
+std::unique_ptr<ept::memory_map> m_mem_map;
+bool m_have_trapped_write_violation = false;
+
+void enable_ept()
+    {
+        m_mem_map = std::make_unique<ept::memory_map>();
+
+        for (auto i = 0ULL; i < page_count; i++) {
+            auto addr = i * page_size_bytes;
+            ept::identity_map_1g(*m_mem_map, addr);
+            auto &entry = m_mem_map->gpa_to_epte(addr);
+
+            ept::epte::read_access::enable(entry);
+            ept::epte::write_access::enable(entry);
+            ept::epte::execute_access::disable(entry);
+        }
+
+        auto eptp = ept::eptp(*m_mem_map);
+        vmcs::ept_pointer::set(eptp);
+        vmcs::secondary_processor_based_vm_execution_controls::enable_ept::enable();
+    }
+
+void register_ept_handlers()
+    {
+        auto hve = this->hve();
+
+        hve->add_ept_read_violation_handler(
+            eapis::intel_x64::ept_violation::handler_delegate_t::create<vcpu, &vcpu::handle_read_violation>(this)
+        );
+
+        hve->add_ept_write_violation_handler(
+            eapis::intel_x64::ept_violation::handler_delegate_t::create<vcpu, &vcpu::handle_write_violation>(this)
+        );
+
+        hve->add_ept_execute_violation_handler(
+            eapis::intel_x64::ept_violation::handler_delegate_t::create<vcpu, &vcpu::handle_execute_violation>(this)
+        );
+
+        hve->add_ept_misconfiguration_handler(
+            eapis::intel_x64::ept_misconfiguration::handler_delegate_t::create<vcpu, &vcpu::handle_ept_misconfiguration>(this)
+        );
+
+        hve->ept_misconfiguration()->enable_log();
+        hve->ept_violation()->enable_log();
+    }
+
+    bool handle_ept_misconfiguration(
+        gsl::not_null<vmcs_t *> vmcs,
+        eapis::intel_x64::ept_misconfiguration::info_t &info)
+    {
+        bfignored(vmcs);
+        bfignored(info);
+
+        info.ignore_advance = true;
+        vmcs::secondary_processor_based_vm_execution_controls::enable_ept::disable();
+
+        for (auto i = 0ULL; i < page_count; i++) {
+            auto addr = i * page_size_bytes;
+            auto &entry = m_mem_map->gpa_to_epte(addr);
+
+            ept::epte::read_access::enable(entry);
+            ept::epte::write_access::enable(entry);
+            ept::epte::execute_access::disable(entry);
+        }
+
+        vmcs::secondary_processor_based_vm_execution_controls::enable_ept::enable();
+
+        return true;
+    }
+
+    bool handle_read_violation(
+        gsl::not_null<bfvmm::intel_x64::vmcs *> vmcs,
+        eapis::intel_x64::ept_violation::info_t &info)
+    {
+        bfignored(vmcs);
+        bfignored(info);
+
+        info.ignore_advance = true;
+        vmcs::secondary_processor_based_vm_execution_controls::enable_ept::disable();
+
+        return true;
+    }
+
+    bool handle_write_violation(
+        gsl::not_null<bfvmm::intel_x64::vmcs *> vmcs,
+        eapis::intel_x64::ept_violation::info_t &info)
+    {
+        bfignored(vmcs);
+        bfignored(info);
+
+        if (m_have_trapped_write_violation) {
+            return true;
+        }
+
+        m_have_trapped_write_violation = true;
+        info.ignore_advance = true;
+        vmcs::secondary_processor_based_vm_execution_controls::enable_ept::disable();
+
+        for (auto i = 0ULL; i < page_count; i++) {
+            auto addr = i * page_size_bytes;
+            auto &entry = m_mem_map->gpa_to_epte(addr);
+
+            ept::epte::read_access::disable(entry);
+            ept::epte::write_access::disable(entry);
+            ept::epte::execute_access::enable(entry);
+        }
+
+        vmcs::secondary_processor_based_vm_execution_controls::enable_ept::enable();
+
+        return true;
+    }
+
+    bool handle_execute_violation(
+        gsl::not_null<bfvmm::intel_x64::vmcs *> vmcs,
+        eapis::intel_x64::ept_violation::info_t &info)
+    {
+        bfignored(vmcs);
+        bfignored(info);
+
+        info.ignore_advance = true;
+        vmcs::secondary_processor_based_vm_execution_controls::enable_ept::disable();
+
+        for (auto i = 0ULL; i < page_count; i++) {
+            auto addr = i * page_size_bytes;
+            auto &entry = m_mem_map->gpa_to_epte(addr);
+
+            ept::epte::read_access::enable(entry);
+            ept::epte::write_access::disable(entry);
+            ept::epte::execute_access::enable(entry);
+        }
+
+        vmcs::secondary_processor_based_vm_execution_controls::enable_ept::enable();
+
+        return true;
+    }
+
 public:
 
     using handler_t = bool(gsl::not_null<bfvmm::intel_x64::vmcs *>);
     using handler_delegate_t = delegate<handler_t>;
 
-    vcpu(vcpuid::type id) : bfvmm::intel_x64::vcpu{id}
+    //vcpu(vcpuid::type id) : bfvmm::intel_x64::vcpu{id}
+
+    vcpu(vcpuid::type id) : eapis::intel_x64::vcpu{id}
     {
+	this->register_ept_handlers();
+	this->enable_ept();
+
         exit_handler()->add_handler(
             intel_x64::vmcs::exit_reason::basic_exit_reason::vmcall,
             handler_delegate_t::create<vcpu, &vcpu::_vmcall_handler>(this)
@@ -56,9 +207,6 @@ public:
         uint64_t id = vmcs->save_state()->rax;
 
         if (id == 1) {
-
-            auto hve = std::make_unique<eapis::intel_x64::hve>(exit_handler(), vmcs.get());
-            ept::enable_ept(ept::eptp(*mem_map), hve.get()); // enable ept
 
             bfdebug_info(0, "vmcall handled");
         }
@@ -231,30 +379,29 @@ public:
 
         gpa_t gpa2 = bfvmm::x64::virt_to_phys_with_cr3(addr, ::intel_x64::vmcs::guest_cr3::get());
 
-        auto hva1 = bfvmm::x64::make_unique_map<void>(addr,
+        auto hva1 = bfvmm::x64::make_unique_map<uint64_t>(addr,
                     ::intel_x64::vmcs::guest_cr3::get(),
                     size,
                     ::intel_x64::vmcs::guest_ia32_pat::get());
 
-        auto hva2 = bfvmm::x64::make_unique_map<void>(gpa2);
+        auto hva2 = bfvmm::x64::make_unique_map<uint64_t>(gpa2);
 
         const auto hpa1 = g_mm->virtptr_to_physint(hva1.get());
         const auto hpa2 = g_mm->virtptr_to_physint(hva2.get());
 
-        ept::map_4k(*mem_map, gpa1, hpa1);
-        ept::map_4k(*mem_map, gpa2, hpa2);
-
+        
         // at this point the mapping is as such
 
         // (dummy buffer)addr -> gpa1 -> hva1 -> hpa1
         //                       gpa2 -> hva2 -> hpa2
 
-        // To goal is:
-
-        // (dummy buffer)addr -> gpa2 -> hva2 -> hpa2
-
         // remap - use EPT and switch the pointers?
-    }
+
+
+	ept::unmap(*m_mem_map, gpa1);  // unmap the gpa before mapping it to another hpa
+	ept::map_4k(*m_mem_map, gpa1, hpa2);
+        //ept::map_4k(*mem_map, gpa2, hpa2);
+}
 
     ~vcpu() = default;
 };
